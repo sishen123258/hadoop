d3ae6272b8b0abce1a4d004b46c27ca7c414a15f
tong.yue <tong.yue@hypers.com>
2016-01-19 16:32:28 +0800

    join main

--- src/domain/Join/JoinMR.java
+++ src/domain/Join/JoinMR.java
@@ -1,18 +1,56 @@
 package domain.Join;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
 import java.io.IOException;
 
 /**
- * 当输入路径有对个时
+ * 当输入路径有对个时 MultipleInputs
  */
 public class JoinMR {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        args=new String[]{ "/tong/sogo/split1","/tong/sogo/split2","/tong/sogo_join",};
+
+        Configuration conf=new Configuration();
+        conf.addResource(new Path("/root/IdeaProjects/hadoop/conf/core-site.xml"));
+        conf.addResource(new Path("/root/IdeaProjects/hadoop/conf/yarn-site.xml"));
+        conf.addResource(new Path("/root/IdeaProjects/hadoop/conf/hdfs-site.xml"));
+        conf.addResource(new Path("/root/IdeaProjects/hadoop/conf/mapred-site.xml"));
+        conf.set("mapred.jar","");
+
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 3) {
+            System.err.println("Usage: wordcount <in> <out>");
+            System.exit(2);
+        }
+
+        Job job=new Job();
+        job.setJarByClass(JoinMR.class);
+        //TextInputFormat
+        MultipleInputs.addInputPath(job, new Path(args[0]),TextInputFormat.class, JoinMapper1.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]),TextInputFormat.class, JoinMapper2.class);
+
+        job.setReducerClass(JoinReduce.class);
+        //TextOutputFormat
+        job.setOutputFormatClass(TextOutputFormat.class);
+        TextOutputFormat.setOutputPath(job, new Path(args[2]));
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(NullWritable.class);
+
+        System.exit(job.waitForCompletion(true)?0:1);
 
     }
 
