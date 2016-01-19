370b6f264411d833649effe41f1ba169206508e0
tong.yue <tong.yue@hypers.com>
2016-01-19 17:59:43 +0800

    partition

--- src/domain/partition/PartitionMR.java
+++ src/domain/partition/PartitionMR.java
@@ -1,11 +1,18 @@
 package domain.partition;
 
-import domain.minmax.MinUserIdMR;
-import domain.minmax.MinUserIdMapper;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
 import java.io.IOException;
 import java.text.ParseException;
@@ -18,8 +25,31 @@ import java.util.Date;
  */
 public class PartitionMR {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        args=new String[]{"/tong/sogo", "/tong/sougo/partition"};
 
+        Configuration conf=new Configuration();
+        conf.addResource(new Path("/root/IdeaProjects/hadoop/conf/core-site.xml"));
+        conf.addResource(new Path("/root/IdeaProjects/hadoop/conf/yarn-site.xml"));
+        conf.addResource(new Path("/root/IdeaProjects/hadoop/conf/hdfs-site.xml"));
+        conf.addResource(new Path("/root/IdeaProjects/hadoop/conf/mapred-site.xml"));
+        conf.set("mapred.jar","/root/IdeaProjects/hadoop/out/artifacts/minSogouUserId_jar/hadoop.jar");
+
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 2) {
+            System.err.println("Usage: wordcount <in> <out>");
+            System.exit(2);
+        }
+
+        Job job=new Job(conf,"word count");
+        job.setJarByClass(PartitionMR.class);
+        job.setMapperClass(PartitionMapper.class);
+        job.setReducerClass(PartitionReduce.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(NullWritable.class);
+        FileInputFormat.addInputPath(job,new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job,new Path(otherArgs[1]));
+        System.exit(job.waitForCompletion(true)? 0 : 1);
     }
 
     public static class PartitionMapper extends Mapper<Object,Text,IntWritable,Text>{
@@ -37,6 +67,19 @@ public class PartitionMR {
             }
             int hour=calendar.HOUR;
             context.write(new IntWritable(hour),value);
+        }
+    }
+
+    public static class PartitionReduce extends Reducer<IntWritable,Text,Text,NullWritable>{
+
+        @Override
+        protected void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+
+            for (Text text: values){
+                //context.write 每执行一次就是一条记录写到文件中
+                context.write(text,NullWritable.get());
+            }
+
         }
     }
 
